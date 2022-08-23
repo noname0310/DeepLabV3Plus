0b01d8565a5c05db5933692981286d8c5d9d1baa
noname <48761044+noname0310@users.noreply.github.com>
2022-08-23 20:38:33 +0900

    retrain model

--- model/tfjs_modelgen.py
+++ model/tfjs_modelgen.py
@@ -4,7 +4,7 @@ Save model to tensorflowjs format.
 from typing import Union
 
 from tensorflow import keras
-from constants import IMAGE_SIZE, NUM_CLASSES
+from constants import IMAGE_SIZE, NUM_CLASSES, MODEL_DIR
 from create_model import deeplab_v3_plus
 
 import tensorflowjs as tfjs
@@ -13,5 +13,7 @@ KerasTensor = Union[keras.layers.Layer, keras.layers.InputLayer]
 
 model: keras.Model = deeplab_v3_plus(
     image_size=IMAGE_SIZE, num_classes=NUM_CLASSES)
+
+model.load_weights(MODEL_DIR)
 
 tfjs.converters.save_keras_model(model, "../webapp/public/tfjs-model")
