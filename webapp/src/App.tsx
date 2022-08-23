2b17be9e01a3c7b12d1208846c420a8e86e44b28
noname <48761044+noname0310@users.noreply.github.com>
2022-08-23 18:50:48 +0900

    use model wip

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -25,6 +25,40 @@ const screenShotDimensions = {
     height: 256
 };
 
+const colorMap = tf.tensor2d([[0, 0, 0], [0, 0, 255]]);
+
+function decodeSegmentationMasks(
+    mask: tf.Tensor<tf.Rank.R2>,
+    colorMap: tf.Tensor<tf.Rank.R2>,
+    classCount: number
+): tf.Tensor<tf.Rank.R2> {
+    const red = tf.zerosLike(mask).asType("int32");
+    const green = tf.zerosLike(mask).asType("int32");
+    const blue = tf.zerosLike(mask).asType("int32");
+
+    for (let i = 0; i < classCount; i++) {
+        const color = colorMap.gather(i);
+        const maskEqualToClass = tf.equal(mask, i);
+
+        const colorRed = color.gather(0);
+        const colorGreen = color.gather(1);
+        const colorBlue = color.gather(2);
+
+        red.add(tf.mul(maskEqualToClass, colorRed));
+        green.add(tf.mul(maskEqualToClass, colorGreen));
+        blue.add(tf.mul(maskEqualToClass, colorBlue));
+
+        maskEqualToClass.dispose();
+        color.dispose();
+    }
+
+    const stack = tf.stack([red, green, blue], 0) as tf.Tensor<tf.Rank.R2>;
+    red.dispose();
+    green.dispose();
+    blue.dispose();
+    return stack;
+}
+
 function App(): JSX.Element {
     const [ model, setModel ] = useState<tf.LayersModel | null>(null);
     const webcamRef = useRef<Webcam>(null);
@@ -45,12 +79,18 @@ function App(): JSX.Element {
         image.src = imageSrc;
         image.onload = async (): Promise<void> => {
             const tensor = tf.browser.fromPixels(image);
-            // print dimensions of tensor
-            console.log(tensor.shape);
+            const tensorExpanded = tf.expandDims(tensor, 0);
+            const prediction = model.predict(tensorExpanded) as tf.Tensor<tf.Rank.R4>;
+            const predictionSqueezed = tf.squeeze(prediction);
+            const predictionArgMax = tf.argMax(predictionSqueezed, 2) as tf.Tensor<tf.Rank.R2>;
+            const predictionColorMap = decodeSegmentationMasks(predictionArgMax, colorMap, 2);
 
-            // const prediction = model.predict(tensor);
-            
-            // console.log(prediction);
+            predictionColorMap.dispose();
+            predictionArgMax.dispose();
+            predictionSqueezed.dispose();
+            prediction.dispose();
+            tensorExpanded.dispose();
+            tensor.dispose();
         };
     });
 
