c33cda4d2b602a3f792c87693d110bdb2c98b73d
noname <48761044+noname0310@users.noreply.github.com>
2022-08-23 16:48:41 +0900

    model wiip

--- webapp/src/deeplabv3plus.ts
+++ webapp/src/deeplabv3plus.ts
@@ -1,5 +1,26 @@
-import { Tensor } from "@tensorflow/tfjs";
+import * as tf from "@tensorflow/tfjs";
+import { LayerArgs } from "@tensorflow/tfjs-layers/dist/engine/topology";
 
-function convolution_block(
-    block_input: Tensor,
-)
+export class TFOpLambda extends tf.layers.Layer {
+    public static className = "TFOpLambda";
+
+    public constructor(config?: LayerArgs) {
+        super(config);
+    }
+
+    public override computeOutputShape(inputShape: tf.Shape | tf.Shape[]): tf.Shape | tf.Shape[] {
+        return inputShape;
+    }
+
+    public override call(_inputs: tf.Tensor | tf.Tensor[], _kwargs: any): tf.Tensor | tf.Tensor[] {
+        return tf.tidy(() => {
+            return tf.scalar(1.0);
+        });
+    }
+}
+
+tf.serialization.registerClass(TFOpLambda);
+
+export function loadDeepLabV3Plus(): Promise<tf.GraphModel> {
+    return tf.loadGraphModel("tfjs-model/model.json");
+}
--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import { loadDeepLabV3Plus } from "./deeplabv3plus";
 
-
+const model = loadDeepLabV3Plus();
+model;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
