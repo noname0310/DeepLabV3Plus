91716ada41cc4e6a4dac7365a88390d4fcc0723f
noname <48761044+noname0310@users.noreply.github.com>
2022-08-23 18:02:02 +0900

    remove test model load

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
-import { loadDeepLabV3Plus } from "./deeplabv3plus";
-
-const model = loadDeepLabV3Plus();
-model;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
