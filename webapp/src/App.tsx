6bb06d001642c0e271c9f9076c0c0314093613ce
noname <48761044+noname0310@users.noreply.github.com>
2022-08-23 15:02:27 +0900

    Update App.tsx

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,20 +1,19 @@
+import styled from "styled-components";
+
+const OuterDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+`;
+
 function App(): JSX.Element {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-          Learn React
-                </a>
-            </header>
-        </div>
+        <OuterDiv>
+            <h1>Hello World</h1>
+        </OuterDiv>
     );
 }
 
