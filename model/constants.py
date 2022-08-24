6e290570ba77961ab6c9b9cf6e5202e58d00bc1f
noname <48761044+noname0310@users.noreply.github.com>
2022-08-25 08:29:53 +0900

    change constants

--- model/constants.py
+++ model/constants.py
@@ -7,8 +7,8 @@ IMAGE_SIZE = 512 // 2
 BATCH_SIZE = 4
 NUM_CLASSES = 2
 DATA_DIR = "./unreal-car-dataset"
-NUM_TRAIN_IMAGES = 1000
-NUM_VAL_IMAGES = 500
+NUM_TRAIN_IMAGES = 1482
+NUM_VAL_IMAGES = 741
 
 MODEL_WEIGHT_DIR = "./deeplabv3plus_weights.h5"
 MODEL_DIR = "./deeplabv3plus_model.h5"
